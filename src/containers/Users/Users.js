15e172abd811d99d17977f4915bebe9bcd28b685
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 17:02:53 +0200

    - added route to users page

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import {Route} from "react-router-dom";
 
 class Users extends Component {
     render () {
         return (
             <div>
-                <h1>The Users Page</h1>
+                <Route path="/users" />
+                <Route render={()=> <h1>The Users Page</h1> }  />
             </div>
         );
     }
